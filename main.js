5561e8e06a92a26efd041e6e159aa35bd374e47c
Selvinjr99 <103534433+Selvinjr99@users.noreply.github.com>
2022-10-14 01:45:28 -0400

    layout website with comments

--- main.js
+++ main.js
@@ -1,21 +1,28 @@
+// SHOW MENU
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
-    
-    // Validate that variables exist
     if(toggle && nav){
         toggle.addEventListener('click', ()=>{
-            // We add the show-menu class to the div tag with the nav__menu class
+            
             nav.classList.toggle('show-menu')
         })
     }
 }
 showMenu('nav-toggle','nav-menu')
 
-/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+
+// CHANGE BACKGROUND HEADER
 function scrollHeader(){
     const nav = document.getElementById('header')
-    // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
     if(this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
+
+
+// Scoll Top 
+function scrollTop(){
+    const scrollTop = document.getElementById('scroll-top');
+    if(this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollTop)
